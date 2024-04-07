efa6e663c4f46b604c5f782c92de15b5e4e9f75f
Gustavo Dantas <gusgalot3g@gmail.com>
2024-04-06 23:36:43 -0300

    feat(lexer): add ast print on Main.java with example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import Parser.ArithmeticExpressionLexer;
+import Parser.ArithmeticExpressionParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        String filename = System.getProperty("user.dir") + "/src/token_example.txt";
+        System.out.println(filename);
+        try {
+            CharStream input = CharStreams.fromFileName(filename);
+            ArithmeticExpressionLexer lexer = new ArithmeticExpressionLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ArithmeticExpressionParser parser = new ArithmeticExpressionParser(tokens);
+
+            ParseTree ast = parser.start();
+
+            System.out.println(ast.toStringTree());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
